06439317a6f729db8d87a979e4927baab7939946
Muhammad Irfan <muhammaddirfan17@gmail.com>
2026-05-09 20:30:49 +0800

    Implementasi Navbar (Desktop & Mobile) + alpine.js

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profil-desa/', views.village_profile, name='profile'),
+    path('profil-desa/', views.village_profile, name='profil_desa'),
     path('keanggotaan/', views.membership, name='anggota'),
 ]
